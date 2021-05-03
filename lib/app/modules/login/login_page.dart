18f5b7e1bf99a0c7fd1c0b4682064e56b9352b83
Luciana Menezes <luciana.menezes@superlogica.com>
2021-05-03 18:50:03 -0300

    login page layout

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,26 +1,122 @@
+import 'dart:io';
+import 'package:cuidapet_fornecedor/app/shared/components/cuidapet_textformfield.dart';
+import 'package:cuidapet_fornecedor/app/shared/theme_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/screenutil.dart';
 import 'login_controller.dart';
 
 class LoginPage extends StatefulWidget {
-  final String title;
-  const LoginPage({Key key, this.title = "Login"}) : super(key: key);
-
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends ModularState<LoginPage, LoginController> {
-  //use 'controller' variable to access controller
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      backgroundColor: ThemeUtils.primaryColor,
+      body: SingleChildScrollView(
+        child: Container(
+          width: ScreenUtil.screenWidthDp,
+          height: ScreenUtil.screenHeightDp,
+          child: Stack(
+            children: [
+              Container(
+                width: ScreenUtil.screenWidthDp,
+                height: ScreenUtil.screenHeightDp < 700
+                    ? 800
+                    : ScreenUtil.screenHeightDp * .95,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage('lib/assets/images/login_background.png'),
+                      fit: BoxFit.fill),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    top: Platform.isIOS
+                        ? ScreenUtil.statusBarHeight + 30
+                        : ScreenUtil.statusBarHeight),
+                //* Double infinity pode ser usado pois o pai está limitando o tamanho da largura
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    Image.asset('lib/assets/images/logo.png',
+                        //*SETWIDTH A LARGURA FICA PROPORCIONAL COM O TAMANHO DO DEVICE
+                        width: ScreenUtil().setWidth(400),
+                        fit: BoxFit.fill),
+                    _buildForm()
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Column(
-        children: <Widget>[],
+    );
+  }
+
+  Widget _buildForm() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Form(
+        key: controller.formKey,
+        child: Column(
+          children: [
+            CuidapetTextFormField(
+                controller: controller.loginController,
+                label: 'Login',
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Login obrigatório';
+                  }
+                  return null;
+                }),
+            SizedBox(height: 20),
+            Observer(builder: (_) {
+              return CuidapetTextFormField(
+                  controller: controller.senhaController,
+                  obscureText: controller.obscureText,
+                  label: 'Senha',
+                  suffixIcon: IconButton(
+                      onPressed: () => controller.showPassword(),
+                      icon: controller.obscureText
+                          ? Icon(Icons.lock)
+                          : Icon(Icons.lock_open)),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Senha obrigatória';
+                    } else if (value.length < 6) {
+                      return 'Senha precisa ter pelo menos 6 caracteres';
+                    }
+                    return null;
+                  });
+            }),
+            Container(
+              width: ScreenUtil.screenWidthDp,
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.all(10),
+              height: 60,
+              child: RaisedButton(
+                onPressed: () => controller.login(),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: ThemeUtils.primaryColor,
+                child: Text('Entrar',
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
