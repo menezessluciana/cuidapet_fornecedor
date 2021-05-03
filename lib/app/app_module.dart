6d24a9bd13c3d8952cc1a47733b85f6c002ff994
Luciana Menezes <luciana.menezes@superlogica.com>
2021-05-03 18:30:34 -0300

    injeção da authstore e adicionando rotas para home e login, definidindo mainpage como initialroute

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,18 +1,26 @@
+import 'package:cuidapet_fornecedor/app/modules/home/home_module.dart';
+import 'package:cuidapet_fornecedor/app/modules/main_page/main_page.dart';
+import 'package:cuidapet_fornecedor/app/shared/auth_store.dart';
 import 'app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:cuidapet_fornecedor/app/app_widget.dart';
-import 'package:cuidapet_fornecedor/app/modules/home/home_module.dart';
+
+import 'modules/login/login_module.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
         Bind((i) => AppController()),
+        Bind((i) => AuthStore()),
       ];
 
   @override
   List<ModularRouter> get routers => [
-        ModularRouter(Modular.initialRoute, module: HomeModule()),
+        ModularRouter(Modular.initialRoute,
+            child: (context, args) => MainPage()),
+        ModularRouter('/home', module: HomeModule()),
+        ModularRouter('/login', module: LoginModule()),
       ];
 
   @override
