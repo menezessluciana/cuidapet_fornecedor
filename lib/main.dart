99813979b0b02209a0d3596a8ad93527e5617dcd
Luciana Menezes <luciana.menezes@superlogica.com>
2021-05-03 18:27:51 -0300

    definindo orientação default do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:cuidapet_fornecedor/app/app_module.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
+void main() async {
+  //* O MAIN EXECUTA ASSIM QUE STARTA A APLICAÇÃO
+  //*Inicializa o app ANTES do runApp
+  WidgetsFlutterBinding.ensureInitialized();
+  //*Definindo apenas uma orientação para o app
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(ModularApp(module: AppModule()));
+}
