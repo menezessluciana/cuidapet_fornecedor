4d5bf794f73b90e22b0206baef4f25aeae0cdf7b
Luciana Menezes <luciana.menezes@superlogica.com>
2021-05-03 18:29:43 -0300

    criando authstore

---
+++ lib/app/shared/auth_store.dart
@@ -0,0 +1,11 @@
+import 'package:mobx/mobx.dart';
+part 'auth_store.g.dart';
+
+class AuthStore = _AuthStoreBase with _$AuthStore;
+
+abstract class _AuthStoreBase with Store {
+  @action
+  Future<bool> isLogged() async {
+    return false;
+  }
+}
